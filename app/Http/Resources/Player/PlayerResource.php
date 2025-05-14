94b41da32e60b45c824c846c8a3876a815987f1a
Yoel Canul <yoel.canul.yacg@gmail.com>
2025-05-14 16:00:29 -0500

    Resources de jugador

---
+++ app/Http/Resources/Player/PlayerResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Player;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PlayerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'curp' => $this->curp,
+            'age' => $this->age,
+            'category' => $this->category,
+            'association_id' => $this->association_id,
+            'created_at' => optional($this->created_at)->toIso8601String(),
+            'updated_at' => optional($this->updated_at)->toIso8601String(),
+        ]; 
+    }
+}
